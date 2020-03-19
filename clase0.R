41e6d8b373e65bf4028e21e5e545a52dd3789191
Gabina <gabinaluz@gmail.com>
2020-03-19 18:04:55 -0300

    Actualizando R y tex

--- clase0.R
+++ clase0.R
@@ -58,37 +58,106 @@ mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , li
 #gráfico de bastones para visitas
 par(mar=c(6,5,4,4), cex = 0.75)
 v <- c(0,as.vector(table(Visitas)),0)
-plot(0:6,v, "h", ylim = c(0,25), ylab = "Frecuencia absoluta", xlab = "Número de visitas", main = "NÚMERO DE VISITAS POR PACIENTE \n ARGENTINA, OCTUBRE 2012")
-mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4.5, at=1., cex = 0.7)
+plot(0:6,v, "h", ylim = c(0,25), ylab = "Frecuencia absoluta", xlab = "Número de visitas", main = "NÚMERO DE VISITAS POR PACIENTE \n ARGENTINA, OCTUBRE 2012", cex.main = 1, cex.lab = 1, font.lab = 2,
+     las = 1)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4.5, at=1.5, cex = 0.7)
 
 #gráfico escalonado para visitas
 vcum <- cumsum(v)/59
-plot(0:6,vcum, "s", ylim = c(0,1), ylab = "Frecuencia relativa acumulada", xlab = "Número de visitas", main = "NÚMERO DE VISITAS POR PACIENTE \n ARGENTINA, OCTUBRE 2012")
-mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4.5, at=1., cex = 0.7)
+plot(0:6,vcum, "s", ylim = c(0,1), ylab = "", xlab = "Número de visitas", main = "NÚMERO DE VISITAS POR PACIENTE \n ARGENTINA, OCTUBRE 2012", 
+     cex.main = 1,
+     font.lab = 2,
+     las = 1)
+abline(h = seq(0,1,by = 0.2), col = "gray67", lty = "dotted")
+mtext(side = 2, "Frecuencia relativa acumulada", font = 2, cex = 0.8, line = 2.5)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4.5, at=1.5, cex = 0.7)
 
 #histograma para edad
-png("hist.png")
-par(mar=c(6,5,5,5))
+#png("hist.png")
+par(mar=c(4,4,4,2),mgp=c(1.8,0.6,0))
 hist(Edad,breaks= seq(11,36,by=3), xlim = c(11,36), right = FALSE, axes = FALSE,
-ylab = "Frecuencia absoluta", xlab = "Edad", font.lab = 2,
-mgp=c(2.5,2,0), col = "lightpink",
-main = "EDAD DE LOS PACIENTES CON ANOREXIA\n ARGENTINA, OCTUBRE 2012")
-axis(side = 1, at =seq(11,36,by=3) )   
-axis(side = 2, at= seq(0,20, by =5)) 
-mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4, 
-at=14, cex = 0.7)
-dev.off()
+ylab = "Frecuencia absoluta", xlab = "Edad", font.lab = 2, col = "lightpink", 
+cex.lab = 0.8, main = NULL)
+axis(side = 1, at =seq(11,36,by=3), cex.axis = 0.8, lwd.ticks = 1, lwd = 1)   
+axis(side = 2, at= seq(0,20, by =5), cex.axis = 0.8,
+las = 1, lwd.ticks = 1, lwd = 1) 
+mtext(side = 3, 
+"EDAD DE LOS PACIENTES CON ANOREXIA\n ARGENTINA, OCTUBRE 2012",
+cex = 0.8, font = 2, line = 1, at = 22)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 3, 
+at=16, cex = 0.7)
+#dev.off()
 
 #polígono de frecuencias para edad
-png("frecuencia.png")
+#png("frecuencia.png")
 e <- as.vector(t(ed[2]))
-plot(seq(11,36,by=3),c(0,e)/59,"l",axes=FALSE, xlab = "Edad", ylab = "Frecuencia relativa")
-axis(side = 1, at =seq(11,36,by=3) )   
-axis(side = 2, at= seq(0,0.5, by =0.1)) 
-dev.off()
+par(mar=c(3.5,5,4,2),mgp=c(2,0.6,0))
+plot(seq(11,36,by=3),c(0,e)/59,"l",axes=FALSE, xlab = "", ylab = "")
+axis(side = 1, at =seq(11,36,by=3),cex.axis = 0.8, padj = -0.5 )   
+axis(side = 2, at= seq(0,0.35, by =0.05),cex.axis = 0.8,
+las = 1, hadj = 1)
+abline(h = seq(0,0.35, by =0.05), col = "gray67", lty = "dotted")
+mtext(side = 1, "Edad", font = 2, cex = 0.8, line = 1.5)
+mtext(side = 2, "Frecuencia relativa", font = 2, cex = 0.8, line = 2.5)
+mtext(side = 3, 
+"EDAD DE LOS PACIENTES CON ANOREXIA\n ARGENTINA, OCTUBRE 2012",
+cex = 0.8, font = 2, line = 1, at = 22)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 2.5, 
+at=16, cex = 0.7)
+#dev.off()
 
 #acumulativo para edad
-plot(seq(11,36,by=3),c(0,cumsum(e)/59),"l",axes=FALSE, xlab = "Edad", ylab = "Frecuencia relativa acumulada")
-axis(side = 1, at =seq(11,36,by=3) )   
-axis(side = 2, at= seq(0,1, by =0.2)) 
+plot(seq(11,36,by=3),c(0,cumsum(e)/59),"l",axes=FALSE, xlab = "", ylab = "")
+axis(side = 1, at =seq(11,36,by=3), cex.axis = 0.8, 
+     padj = -0.5)
+axis(side = 2, at= seq(0,1, by =0.2), las = 1, cex.axis = 0.8,
+     hadj = 1)
+abline(h = seq(0,1, by =0.2), col = "gray67", lty = "dotted")
+mtext(side = 1, "Edad", font = 2, cex = 0.8, line = 1.5)
+mtext(side = 2, "Frecuencia relativa acumulada", font = 2, cex = 0.8, line = 2.5)
+mtext(side = 3, 
+      "EDAD DE LOS PACIENTES CON ANOREXIA\n ARGENTINA, OCTUBRE 2012",
+      cex = 0.8, font = 2, line = 1, at = 22)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 2.5, 
+      at=16, cex = 0.7)
 
+#boxplot
+
+mean(Edad)
+median(Edad)
+min(Edad)
+max(Edad)
+summary(Edad)
+summary(Sexo)
+summary(Visitas)
+summary(Signo)
+
+par(font.lab = 2)
+boxplot(x = Edad, horizontal = TRUE, xlab = "Edad",
+        ylim = c(10,35),
+        right = FALSE,
+        font.lab =2, cex.lab = 1, col = "moccasin")
+par(font.lab = 1)
+mtext(side = 3, 
+      "EDAD DE LOS PACIENTES CON ANOREXIA\n ARGENTINA, OCTUBRE 2012",
+      cex = 0.8, font = 2, line = 1, at = 22)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4, 
+      at=16, cex = 0.7)
+
+#boxplot comparativo
+par(font.lab = 2)
+boxplot(formula = Edad~Sexo, xlab = "Sexo",
+        ylab = "",
+        ylim = c(10,35),
+        right = FALSE,
+        boxwex = 0.4,
+        font.lab =2, cex.lab = 1, col = "moccasin",
+        las = 1,
+        main =  "EDAD SEGÚN SEXO DE LOS PACIENTES CON ANOREXIA\n ARGENTINA, OCTUBRE 2012",
+        cex.main = 1)
+par(font.lab = 1)
+mtext(side = 2, 
+      "Edad",
+      cex = 0.8, font = 2, line = 2.5, at = 22)
+mtext(side=1,"Fuente: Asociación de Lucha contra la Bulimia y la Anorexia" , line = 4, 
+      at=1, cex = 0.7)
